d8c04d98906ed5652977a76192a05c80a8f6b3cd
Brad Ross <brad.ross.35@gmail.com>
2015-03-30 19:41:09 -0700

    added error handling for failed indexing

--- app/index/index.go
+++ app/index/index.go
@@ -2,7 +2,7 @@ package index
 
 import (
 	"encoding/json"
-	//"errors"
+	"errors"
 	"log"
 	"net/http"
 
@@ -30,7 +30,7 @@ func InitIndexRequestHandler(server *mux.Router, config *configVars.Configuratio
 	indexingJobQueue = indexingJobQueueObj
 	indexingJobCompletionQueue = indexingJobCompletionQueueObj
 
-	server.HandleFunc("/index", handleIndexRequest(config)).Methods("POST")
+	server.HandleFunc("/index", handleIndexRequest).Methods("POST")
 
 	return nil
 }
@@ -44,65 +44,79 @@ type AlreadyIndexedResponse struct {
 	AlreadyIndexed bool `json:"alreadyIndexed"`
 }
 
-func handleIndexRequest(config *configVars.Configuration) func(http.ResponseWriter, *http.Request) {
-	return func(rw http.ResponseWriter, req *http.Request) {
-		var indexRequest IndexRequest
-		parseErr := json.NewDecoder(req.Body).Decode(&indexRequest)
-		if parseErr != nil {
-			rw.WriteHeader(400)
-			log.Panicln(parseErr)
-			return
-		}
-
-		//Checks to see if a volunteer with this userId already exists. If so,
-		//the user has already been indexed. If not, the user still needs to be indexed.
-		volunteer, volunteerSearchErr := graph.FindVolunteer(indexRequest.UserId)
-		if volunteerSearchErr != nil {
-			rw.WriteHeader(400)
-			log.Panicln(volunteerSearchErr)
-			return
-		} else if volunteer != nil {
-			json.NewEncoder(rw).Encode(AlreadyIndexedResponse{AlreadyIndexed: true})
-			return
-		}
-
-		longTermToken, _, exchangeErr := facebook.GetLongTermToken(indexRequest.AccessToken)
-		if exchangeErr != nil {
-			rw.WriteHeader(400)
-			log.Panicln(exchangeErr)
-			return
-		}
-
-		indexingJobQueue.PushMessage("INDEX_REQUEST", IndexRequest{UserId: indexRequest.UserId, AccessToken: longTermToken})
-
-		indexingErr := waitForIndexingCompletion(indexRequest.UserId)
-		if indexingErr != nil {
-			rw.WriteHeader(400)
-			log.Panicln(indexingErr)
-			return
-		}
-
-		rw.WriteHeader(200)
+func handleIndexRequest(rw http.ResponseWriter, req *http.Request) {
+	var indexRequest IndexRequest
+	parseErr := json.NewDecoder(req.Body).Decode(&indexRequest)
+	if parseErr != nil {
+		rw.WriteHeader(400)
+		log.Panicln(parseErr)
+		return
 	}
+
+	//Checks to see if a volunteer with this userId already exists. If so,
+	//the user has already been indexed. If not, the user still needs to be indexed.
+	volunteer, volunteerSearchErr := graph.FindVolunteer(indexRequest.UserId)
+	if volunteerSearchErr != nil {
+		rw.WriteHeader(400)
+		log.Panicln(volunteerSearchErr)
+		return
+	} else if volunteer != nil {
+		json.NewEncoder(rw).Encode(AlreadyIndexedResponse{AlreadyIndexed: true})
+		return
+	}
+
+	longTermToken, _, exchangeErr := facebook.GetLongTermToken(indexRequest.AccessToken)
+	if exchangeErr != nil {
+		rw.WriteHeader(400)
+		log.Panicln(exchangeErr)
+		return
+	}
+
+	indexingJobQueue.PushMessage("INDEX_REQUEST", IndexRequest{UserId: indexRequest.UserId, AccessToken: longTermToken})
+
+	indexingErr := waitForIndexingCompletion(indexRequest.UserId)
+	if indexingErr != nil {
+		rw.WriteHeader(400)
+		log.Panicln(indexingErr)
+		return
+	}
+
+	rw.WriteHeader(200)
 }
 
 func waitForIndexingCompletion(userId string) error {
 	indexingComplete := make(chan bool)
+	var indexingErr error
 
 	successCallbackId := indexingJobCompletionQueue.RegisterCallback("SUCCESS", func(userId string, indexingComplete chan bool) func(map[string]interface{}) {
 		//the callback function is returned from a closure to pass the userId and the channel to notify completion of indexing
 		return func(payload map[string]interface{}) {
 			if payload["userId"] == userId {
 				indexingComplete <- true
+
 			}
 		}
 	}(userId, indexingComplete))
-	//TODO: register errorCallback and return and error
+
+	failureCallbackId := indexingJobCompletionQueue.RegisterCallback("FAILURE", func(userId string, indexingComplete chan bool, indexingErr *error) func(map[string]interface{}) {
+		//the callback function is returned from a closure to pass the userId and the channel to notify completion of indexing
+		return func(payload map[string]interface{}) {
+			if payload["userId"] == userId {
+				*indexingErr = errors.New(payload["message"].(string))
+				indexingComplete <- true
+			}
+		}
+	}(userId, indexingComplete, &indexingErr))
 
 	//blocks until indexingComplete recieves true from the callback function
 	<-indexingComplete
 
+	if indexingErr != nil {
+		return indexingErr
+	}
+
 	indexingJobCompletionQueue.UnregisterCallback(successCallbackId)
+	indexingJobCompletionQueue.UnregisterCallback(failureCallbackId)
 
 	return nil
 }
