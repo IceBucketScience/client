3dd36ae90672b53f1b8994a488c379aa3a94fc34
Brad Ross <brad.ross.35@gmail.com>
2015-04-30 17:34:00 -0700

    removed index button and graph display

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -329,60 +329,23 @@ var Fluxxor = require("fluxxor");
 var FluxMixin = Fluxxor.FluxMixin(React);
 var StoreWatchMixin = Fluxxor.StoreWatchMixin;
 
-var Fb = require("fb");
-
-Fb.init({
-    appId: "155173904571953",
-    version: "v1.0",
-    status: true,
-    cookie: true,
-    xfbml: true
-});
-
-var SessionStore = require("./stores/SessionStore");
-var GraphPlayerStore = require("./stores/GraphPlayerStore");
-var GraphStore = require("./stores/GraphStore");
 var SurveyStore = require("./stores/SurveyStore");
 
 var stores = {
-    SessionStore: new SessionStore(),
-    GraphPlayerStore: new GraphPlayerStore(),
-    GraphStore: new GraphStore(),
     SurveyStore: new SurveyStore()
 };
 
-var SessionActions = require("./actions/SessionActions");
-var GraphPlayerActions = require("./actions/GraphPlayerActions");
 var SurveyActions = require("./actions/SurveyActions");
 
 var actions = {
-    session: SessionActions,
-    graphPlayer: GraphPlayerActions,
     survey: SurveyActions
 };
 
 var Block = require("./components/Block.jsx");
-var FbLoginPanel = require("./components/FbLoginPanel.jsx");
-var GraphPlayer = require("./components/GraphPlayer.jsx");
 var Survey = require("./components/Survey.jsx");
 
 var App = React.createClass({displayName: "App",
-    mixins: [FluxMixin, StoreWatchMixin("SessionStore", "GraphStore")],
-    getInitialState: function() {
-        return {
-            graphShown: false
-        };
-    },
-    getStateFromFlux: function() {
-        var flux = this.getFlux();
-
-        return {
-            session: flux.store("SessionStore").getState()
-        };
-    },
-    handleShowGraph: function() {
-        this.setState({graphShown: true});
-    },
+    mixins: [FluxMixin],
     render: function() {
         return React.createElement("div", {className: "container"}, 
                 React.createElement("div", {className: "row"}, 
@@ -393,12 +356,7 @@ var App = React.createClass({displayName: "App",
                                 React.createElement("img", {className: "img-responsive img-circle", src: "/static/assets/brad_photo.jpg", style: {width: "100%", marginLeft: "auto", marginRight: "auto"}})
                             ), 
                             React.createElement("div", {className: "col-sm-9"}, 
-                                React.createElement("h4", {className: "lead"}, "I'm Brad Ross, and for my high school senior thesis project, I want to explore this topic by studying the ", React.createElement("a", {href: "http://www.alsa.org/fight-als/ice-bucket-challenge.html"}, "#ALSIceBucketChallenge"), ". To do so, ", React.createElement("strong", null, "I need you to answer a few questions and allow a program I wrote to analyze posts on your 5/14 - 9/14 News Feed."), " I promise I'll protect your privacy--see below for details.")
-                            )
-                        ), 
-                        React.createElement("div", {className: "row"}, 
-                            React.createElement("div", {className: "col-sm-12"}, 
-                                React.createElement("h4", {className: "lead"}, "My access to Facebook data will disappear in less than two weeks, so ", React.createElement("strong", null, "I need you to let my program analyze your network in the next few days"), " in order to complete my work. If you're interested in seeing the results, shoot me an email at brad.ross.35@gmail.com. Thanks in advance!")
+                                React.createElement("h4", {className: "lead"}, "I'm Brad Ross, and for my high school senior thesis project, I want to explore this topic by studying the ", React.createElement("a", {href: "http://www.alsa.org/fight-als/ice-bucket-challenge.html"}, "#ALSIceBucketChallenge"), ". To do so, ", React.createElement("strong", null, "I need you to answer the questions below."), " I promise I will not make your data public, and I will permanently delete your responses after I'm done with my project. If you're interested in seeing the results, shoot me an email at brad.ross.35@gmail.com. Thanks in advance!")
                             )
                         )
                     )
@@ -406,30 +364,10 @@ var App = React.createClass({displayName: "App",
 
                 React.createElement("div", {className: "row"}, 
                     React.createElement(Block, {
-                        size: "6", 
-                        description: "Index Your Facebook"}, 
-                        React.createElement(FbLoginPanel, {session: this.state.session})
-                    ), 
-
-                    React.createElement(Block, {
-                        size: "6", 
+                        size: "10", 
+                        offset: "1", 
                         description: "Fill Out This Short Survey"}, 
                         React.createElement(Survey, null)
-                    )
-                ), 
-
-                React.createElement("div", {className: "row"}, 
-                    React.createElement(Block, {
-                        size: "12", 
-                        description: "Explore the Ice Bucket Challenge in Your Network"}, 
-                        React.createElement(GraphPlayer, null)
-                    )
-                ), 
-
-                React.createElement("div", {className: "row"}, 
-                    React.createElement("div", {className: "col-sm-8 col-sm-offset-2 text-center"}, 
-                        React.createElement("h1", null, "Isn't This Cool?"), 
-                        React.createElement("h3", {className: "lead"}, "If you think so (I certainly do), ", React.createElement("strong", null, "please share this with your friends"), " through Facebook or another social media platform of choice.")
                     )
                 )
             );
@@ -440,7 +378,7 @@ var flux = new Fluxxor.Flux(stores, actions);
 
 React.render(React.createElement(App, {flux: flux}), document.getElementById("app"));
 
-},{"./actions/GraphPlayerActions":2,"./actions/SessionActions":3,"./actions/SurveyActions":4,"./components/Block.jsx":6,"./components/FbLoginPanel.jsx":7,"./components/GraphPlayer.jsx":10,"./components/Survey.jsx":12,"./stores/GraphPlayerStore":17,"./stores/GraphStore":18,"./stores/SessionStore":19,"./stores/SurveyStore":20,"fb":1,"fluxxor":23,"react":"react"}],6:[function(require,module,exports){
+},{"./actions/SurveyActions":4,"./components/Block.jsx":6,"./components/Survey.jsx":12,"./stores/SurveyStore":20,"fluxxor":23,"react":"react"}],6:[function(require,module,exports){
 var React = require("react");
 
 module.exports = React.createClass({displayName: "exports",
--- client/js/app.jsx
+++ client/js/app.jsx
@@ -3,60 +3,23 @@ var Fluxxor = require("fluxxor");
 var FluxMixin = Fluxxor.FluxMixin(React);
 var StoreWatchMixin = Fluxxor.StoreWatchMixin;
 
-var Fb = require("fb");
-
-Fb.init({
-    appId: "155173904571953",
-    version: "v1.0",
-    status: true,
-    cookie: true,
-    xfbml: true
-});
-
-var SessionStore = require("./stores/SessionStore");
-var GraphPlayerStore = require("./stores/GraphPlayerStore");
-var GraphStore = require("./stores/GraphStore");
 var SurveyStore = require("./stores/SurveyStore");
 
 var stores = {
-    SessionStore: new SessionStore(),
-    GraphPlayerStore: new GraphPlayerStore(),
-    GraphStore: new GraphStore(),
     SurveyStore: new SurveyStore()
 };
 
-var SessionActions = require("./actions/SessionActions");
-var GraphPlayerActions = require("./actions/GraphPlayerActions");
 var SurveyActions = require("./actions/SurveyActions");
 
 var actions = {
-    session: SessionActions,
-    graphPlayer: GraphPlayerActions,
     survey: SurveyActions
 };
 
 var Block = require("./components/Block.jsx");
-var FbLoginPanel = require("./components/FbLoginPanel.jsx");
-var GraphPlayer = require("./components/GraphPlayer.jsx");
 var Survey = require("./components/Survey.jsx");
 
 var App = React.createClass({
-    mixins: [FluxMixin, StoreWatchMixin("SessionStore", "GraphStore")],
-    getInitialState: function() {
-        return {
-            graphShown: false
-        };
-    },
-    getStateFromFlux: function() {
-        var flux = this.getFlux();
-
-        return {
-            session: flux.store("SessionStore").getState()
-        };
-    },
-    handleShowGraph: function() {
-        this.setState({graphShown: true});
-    },
+    mixins: [FluxMixin],
     render: function() {
         return <div className="container">
                 <div className="row">
@@ -67,12 +30,7 @@ var App = React.createClass({
                                 <img className="img-responsive img-circle" src="/static/assets/brad_photo.jpg" style={{width: "100%", marginLeft: "auto", marginRight: "auto"}}/>
                             </div>
                             <div className="col-sm-9">
-                                <h4 className="lead">I'm Brad Ross, and for my high school senior thesis project, I want to explore this topic by studying the <a href="http://www.alsa.org/fight-als/ice-bucket-challenge.html">#ALSIceBucketChallenge</a>. To do so, <strong>I need you to answer a few questions and allow a program I wrote to analyze posts on your 5/14 - 9/14 News Feed.</strong> I promise I'll protect your privacy--see below for details.</h4>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-sm-12">
-                                <h4 className="lead">My access to Facebook data will disappear in less than two weeks, so <strong>I need you to let my program analyze your network in the next few days</strong> in order to complete my work. If you're interested in seeing the results, shoot me an email at brad.ross.35@gmail.com. Thanks in advance!</h4>
+                                <h4 className="lead">I'm Brad Ross, and for my high school senior thesis project, I want to explore this topic by studying the <a href="http://www.alsa.org/fight-als/ice-bucket-challenge.html">#ALSIceBucketChallenge</a>. To do so, <strong>I need you to answer the questions below.</strong> I promise I will not make your data public, and I will permanently delete your responses after I'm done with my project. If you're interested in seeing the results, shoot me an email at brad.ross.35@gmail.com. Thanks in advance!</h4>
                             </div>
                         </div>
                     </div>
@@ -80,31 +38,11 @@ var App = React.createClass({
 
                 <div className="row">
                     <Block
-                        size="6"
-                        description="Index Your Facebook">
-                        <FbLoginPanel session={this.state.session} />
-                    </Block>
-
-                    <Block
-                        size="6"
+                        size="10"
+                        offset="1"
                         description="Fill Out This Short Survey">
                         <Survey />
                     </Block>
-                </div>
-
-                <div className="row">
-                    <Block
-                        size="12"
-                        description="Explore the Ice Bucket Challenge in Your Network">
-                        <GraphPlayer />
-                    </Block>
-                </div>
-
-                <div className="row">
-                    <div className="col-sm-8 col-sm-offset-2 text-center">
-                        <h1>Isn't This Cool?</h1>
-                        <h3 className="lead">If you think so (I certainly do), <strong>please share this with your friends</strong> through Facebook or another social media platform of choice.</h3>
-                    </div>
                 </div>
             </div>;
     }
